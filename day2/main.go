8b443ef84a637ee77e855586f11262226a2c88f7
Iamsheye <seyepelumi@gmail.com>
2022-10-07 00:36:15 +0100

    if check fix

--- day2/main.go
+++ day2/main.go
@@ -4,14 +4,14 @@ import "fmt"
 
 func main() {
 	fmt.Println(LeapYear(2195, 3))
-	fmt.Println(LeapYear(2010, 30))
+	fmt.Println(LeapYear(2799, 10))
 }
 
 func LeapYear(year int, num int) []int {
 	leapYears := []int{}
 
 	for i := year + 1; len(leapYears) < num; i++ {
-		if i%4 == 0 && i%100 != 0 {
+		if i%4 == 0 && i%100 != 0 || i%400 == 0 {
 			leapYears = append(leapYears, i)
 		}
 	}
